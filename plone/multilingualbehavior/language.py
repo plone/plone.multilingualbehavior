9d60b95fb2f570fa52af78caf8ced5a4e312e9e6
Victor Fernandez de Alba <sneridagh@gmail.com>
2012-10-02 22:29:34 +0200

    Bring back the patch for setting the language field readonly in edit form.

--- plone/multilingualbehavior/language.py
+++ plone/multilingualbehavior/language.py
@@ -2,6 +2,11 @@ from zope import interface
 
 from plone.multilingual.interfaces import LANGUAGE_INDEPENDENT
 from plone.multilingual.interfaces import ILanguage
+from plone.app.dexterity.behaviors.metadata import ICategorization
+
+
+# Patch for hiding 'language' field from the edit form
+ICategorization['language'].readonly = True
 
 
 class Language(object):
@@ -19,4 +24,3 @@ class Language(object):
 
     def set_language(self, language):
         self.context.language = language
-
