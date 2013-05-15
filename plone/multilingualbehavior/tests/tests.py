cdac56250cc626c5f4fd53c399bdfe4e4884cf4a
Victor Fernandez de Alba <sneridagh@gmail.com>
2013-05-15 17:17:34 +0200

    Conditionally remove one test that would neve have past for Dexterity versions under 2.0

--- plone/multilingualbehavior/tests/tests.py
+++ plone/multilingualbehavior/tests/tests.py
@@ -5,6 +5,9 @@ from plone.multilingualbehavior.testing import PLONEMULTILINGUALBEHAVIOR_INTEGRA
 from plone.multilingualbehavior.testing import PLONEMULTILINGUALBEHAVIOR_FUNCTIONAL_TESTING
 from plone.multilingualbehavior.testing import optionflags
 
+import pkg_resources
+
+
 integration_tests = [
     'doctest_behavior.txt',
     'doctest_native.txt',
@@ -16,7 +19,19 @@ functional_tests = [
 ]
 
 
+def is_plone43():
+    plone_pkg = pkg_resources.get_distribution('Products.CMFPlone')
+    return cmp(pkg_resources.parse_version(plone_pkg.version),
+               pkg_resources.parse_version('4.3')) >= 0
+
+
 def test_suite():
+    if not is_plone43():
+        # This test doesn't work for versions for Dexterity under 2.0
+        # For testing purposes, we only test for the Plone version is
+        # superior to 4.3 as this is the fixture being tested.
+        integration_tests.remove('doctest_manualbehavior.txt')
+
     return unittest.TestSuite(
         [layered(doctest.DocFileSuite('%s' % f,
                     package='plone.multilingualbehavior.tests',
