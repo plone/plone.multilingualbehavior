2cf5aac4f0c1084e33b1a1322e47bc087fbfb15c
Daniel Widerin <daniel@widerin.net>
2013-05-31 13:44:48 +0200

    Update subscriber for modification of translated objects

--- plone/multilingualbehavior/subscriber.py
+++ plone/multilingualbehavior/subscriber.py
@@ -1,18 +1,16 @@
-from zope.component import getUtility
-from zope.component import queryAdapter
-from zope.event import notify
-from zope.lifecycleevent import Attributes
-from zope.lifecycleevent import ObjectModifiedEvent
-from zope.lifecycleevent.interfaces import IObjectModifiedEvent
-
-
-from plone.dexterity.interfaces import IDexterityFTI
-
-from plone.multilingualbehavior.interfaces import IDexterityTranslatable
+# -*- coding: utf-8 -*-
+from AccessControl import getSecurityManager
+from AccessControl.SecurityManagement import newSecurityManager
+from AccessControl.SecurityManagement import setSecurityManager
+from AccessControl.User import UnrestrictedUser
+from Products.CMFCore.utils import getToolByName
 
 from plone.multilingual.interfaces import ILanguage
 from plone.multilingual.interfaces import ILanguageIndependentFieldsManager
 from plone.multilingual.interfaces import ITranslationManager
+from plone.multilingualbehavior.interfaces import IDexterityTranslatable
+from zope.component import queryAdapter
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 
 class LanguageIndependentModifier(object):
@@ -33,33 +31,48 @@ class LanguageIndependentModifier(object):
         else:
             self.stack.append(canonical)
 
-            # Copy over all language independent fields
-            translations = self.get_all_translations(content)
-            manager = ILanguageIndependentFieldsManager(content)
-            for translation in translations:
-                manager.copy_fields(translation)
+            sm = getSecurityManager()
+            acl_users = getToolByName(content, 'acl_users')
 
-            fti = getUtility(IDexterityFTI, name=content.portal_type)
-            schema = fti.lookupSchema()
-            descriptions = Attributes(schema)
-            self.reindex_translations(translations, descriptions)
+            try:
+                # Clone the current user and assign a new editor role to
+                # allow edition of all translated objects even if the
+                # current user whould not have permission to do that.
+                tmp_user = UnrestrictedUser(
+                    sm.getUser().getId(), '', ['Editor', ], '')
+
+                # Wrap the user in the acquisition context of the portal
+                # and finally switch the user to our new editor
+                tmp_user = tmp_user.__of__(acl_users)
+                newSecurityManager(None, tmp_user)
+
+                # Copy over all language independent fields
+                transmanager = ITranslationManager(content)
+                fieldmanager = ILanguageIndependentFieldsManager(content)
+                for translation in self.get_all_translations(content):
+                    trans_obj = transmanager.get_translation(translation)
+                    fieldmanager.copy_fields(trans_obj)
+                    self.reindex_translation(trans_obj)
+            finally:
+                # Restore the old security manager
+                setSecurityManager(sm)
+
             self.stack.remove(canonical)
 
-    def reindex_translations(self, translations, descriptions):
-        """Once the modifications are done, reindex all translations"""
-        for translation in translations:
-            translation.reindexObject()
-            notify(ObjectModifiedEvent(translation, descriptions))
+    def reindex_translation(self, translation):
+        """Once the modification is done, reindex translation"""
+        translation.reindexObject()
+
+        # fti = getUtility(IDexterityFTI, name=translation.portal_type)
+        # schema = fti.lookupSchema()
+        # descriptions = Attributes(schema)
+        # notify(ObjectModifiedEvent(translatio, descriptions))
 
     def get_all_translations(self, content):
         """Return all translations excluding the just modified content"""
-        translations_list_to_process = []
         content_lang = queryAdapter(content, ILanguage).get_language()
-        canonical = ITranslationManager(content)
-        translations = canonical.get_translations()
-        for language in translations.keys():
-            if language != content_lang:
-                translations_list_to_process.append(translations[language])
-        return translations_list_to_process
+        translations = ITranslationManager(content).get_translated_languages()
+        translations.remove(content_lang)
+        return translations
 
 handler = LanguageIndependentModifier()
