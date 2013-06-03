e1ceb46ba399403e58e41f963baf1aacc0d99c4a
Daniel Widerin <daniel@widerin.net>
2013-06-03 11:48:50 +0200

    Rename README.txt to README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.1dev'
 setup(name='plone.multilingualbehavior',
       version=version,
       description="Dexterity behavior for enabling multilingual extensions",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
